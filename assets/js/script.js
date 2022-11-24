a8409375392a0de45766272b26956b07607a476b
taegyung <taegyung.e@gmail.com>
2022-11-24 14:40:54 +0900

    추가/삭제/완료/수정 등등 기능 추가

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,30 +3,62 @@
     const addBtn = document.querySelector('.add_btn');
     addBtn.addEventListener('click', todoAdd);
 
+    document.addEventListener('click', (e) => {
 
-    // - 할일 추가하기
+        const target = e.target;
+        const Li = target.closest('li');
+
+        // 완료 하기
+        todoComplete(Li);
+
+        // 삭제버튼
+        if(target.parentElement.classList.contains('del_btn')) {
+            todoDel(Li);
+        }
+
+        // 수정폼 열기 버튼
+        if(target.parentElement.classList.contains('modify_btn')) {
+            todoModifyOpen(Li);
+        }
+
+        // 수정버튼
+        if(target.classList.contains('finish_btn')) {
+            todoModify(Li);
+        }
+
+        // 수정취소버튼
+        if(target.classList.contains('cancel_btn')) {
+            todoModifyCancel(Li);
+        }
+
+    })
+
+
+    // 추가하기
     function todoAdd() {
         const addInput = document.querySelector('.add_input');
         const list = document.querySelector('.list');
         const listLi = document.querySelectorAll('.list li');
         const li = document.createElement('li');
 
-        const temp = `<div class="cont">
-                            <input type="checkbox" name="" id="todo${listLi.length}">
-                            <label for="todo${listLi.length}">
-                                <span class="check_ico">
-                                    <i class="fa-solid fa-check"></i>
-                                </span>
-                                <span class="txt">${addInput.value}</span>
-                            </label>
-                        </div>
-                        <div class="util">
-                            <button type="button" class="del_btn" title="삭제">
-                                <i class="fa-regular fa-trash-can"></i>
-                            </button>
-                            <button type="button" class="modify_btn" title="수정">
-                                <i class="fa-regular fa-pen-to-square"></i>
-                            </button>
+        const temp = `<div class="view_item">
+                            <div class="cont">
+                                <input type="checkbox" name="" id="todo${listLi.length}">
+                                <label for="todo${listLi.length}">
+                                    <span class="check_ico">
+                                        <i class="fa-solid fa-check"></i>
+                                    </span>
+                                    <span class="txt">${addInput.value}</span>
+                                </label>
+                            </div>
+                            <div class="util">
+                                <button type="button" class="del_btn" title="삭제">
+                                    <i class="fa-regular fa-trash-can"></i>
+                                </button>
+                                <button type="button" class="modify_btn" title="수정">
+                                    <i class="fa-regular fa-pen-to-square"></i>
+                                </button>
+                            </div>
                         </div>`;
         li.innerHTML = temp;
 
@@ -35,19 +67,61 @@
         addInput.value = '';
 
     }
-    // - 할일 삭제하기
-    function todoDel() {
-        console.log('삭제~');
+    
+    // 삭제하기
+    function todoDel(target) {
+        if(confirm('정말로 삭제하시겠습니까?')) {
+            target.remove();
+        }
     }
-    // - 할일 완료하기
-    function todoComplete() {
-        console.log('완료~');
+
+    // 완료하기
+    function todoComplete(target) {
+        if(target) {
+            if(target.querySelector('input[type="checkbox"]').checked) {
+                target.classList.add('complete');
+            } else {
+                target.classList.remove('complete');
+            }
+        }
+    }
+    
+    // 수정 하기
+    function todoModify(target) {
+        const viewItem = target.querySelector('.view_item');
+        const viewItemTxt = viewItem.querySelector('.txt');
+        const Modify = target.querySelector('.type_modify');
+
+        Modify.remove();
+        viewItem.style.display = 'flex';
+        viewItemTxt.innerText = Modify.querySelector('.modify_input').value;
 
     }
-    // - 할일 수정하기
-    function todoUpdate() {
-        console.log('수정~');
 
+    // 수정폼 오픈
+    function todoModifyOpen(target) {
+        const viewItem = target.querySelector('.view_item');
+        const viewItemTxt = viewItem.querySelector('.txt');
+        const inputItem = document.createElement('div');
+        const temp = `<input type="text" name="" id="" class="modify_input" value="${viewItemTxt.innerHTML}">
+                        <button type="button" class="finish_btn btn bg_black">수정</button>
+                        <button type="button" class="cancel_btn btn line_black">취소</button>`;
+        inputItem.classList.add('input_item');
+        inputItem.classList.add('type_modify');
+        inputItem.innerHTML = temp;
+
+        viewItem.style.display = 'none';
+        target.append(inputItem);
+
+    }
+
+    // 수정폼 취소 버튼
+    function todoModifyCancel(target) {
+        const viewItem = target.querySelector('.view_item');
+        const Modify = target.querySelector('.type_modify');
+
+        Modify.remove();
+        viewItem.style.display = 'flex';
     }
 
 

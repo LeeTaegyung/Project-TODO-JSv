8501528c910378e584b12d8fb11f7888c153da01
taegyung <taegyung.e@gmail.com>
2022-11-25 15:45:04 +0900

    localStorage 추가

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@
     const addBtn = document.querySelector('.add_btn');
     addBtn.addEventListener('click', todoAdd);
 
+    init();
+
     document.addEventListener('click', (e) => {
 
         const target = e.target;
@@ -33,22 +35,54 @@
 
     })
 
+    function init() {
+        if(localStorage.length > 0) {
+            // localStorage 키값이 제대로 정렬되지 않는 문제가 있어서 count키를 제외한 key만 따로 담아 정렬.
+            let keys = [];
+            for(let i = 0; i < localStorage.length; i++) {
+                if(localStorage.key(i) === 'count') continue;
+                keys.push(+localStorage.key(i));
+            }
 
-    // 추가하기
-    function todoAdd() {
-        const addInput = document.querySelector('.add_input');
+            // sort 
+            keys.sort((a, b) => a - b);
+
+            console.log(keys);
+
+            for(let i = 0; i < keys.length; i++) {
+                let item = JSON.parse(localStorage.getItem(keys[i]));
+                todoCreate(keys[i], item.txt, item.check);
+            }
+        } else {
+            localStorage.setItem('count', 0);
+        }
+    }
+
+    // 스토리지 추가 / 업데이트
+    function setStorage(key, txt, check) {
+        const value = { txt, check };
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    function todoCreate(key, txt, check) {
         const list = document.querySelector('.list');
-        const listLi = document.querySelectorAll('.list li');
         const li = document.createElement('li');
+
+        if(check) {
+            check = 'checked';
+            li.classList.add('complete');
+        } else {
+            check = '';
+        }
 
         const temp = `<div class="view_item">
                             <div class="cont">
-                                <input type="checkbox" name="" id="todo${listLi.length}">
-                                <label for="todo${listLi.length}">
+                                <input type="checkbox" name="" id="todo${key}" ${check}>
+                                <label for="todo${key}">
                                     <span class="check_ico">
                                         <i class="fa-solid fa-check"></i>
                                     </span>
-                                    <span class="txt">${addInput.value}</span>
+                                    <span class="txt">${txt}</span>
                                 </label>
                             </div>
                             <div class="util">
@@ -63,7 +97,23 @@
         li.innerHTML = temp;
 
         list.append(li);
+        
+    }
 
+    // 추가하기
+    function todoAdd() {
+        const addInput = document.querySelector('.add_input');
+        const txt = addInput.value;
+        const key = +localStorage.getItem('count');
+
+        // 스토리지에 있는 count를 기준으로 키값 부여.
+        // 이렇게 하지 않고 length로 하게 될 경우, 기존의 키값을 덮어씌우는 일이 발생함.
+
+        todoCreate(key, txt, false);
+        setStorage(key, txt, false);
+
+        // count값 업데이트
+        localStorage.setItem('count', key + 1);
         addInput.value = '';
 
     }
@@ -71,6 +121,9 @@
     // 삭제하기
     function todoDel(target) {
         if(confirm('정말로 삭제하시겠습니까?')) {
+            let key = target.querySelector('input[type="checkbox"]').id;
+            key = key.split('todo')[1];
+            localStorage.removeItem(key);
             target.remove();
         }
     }
@@ -78,11 +131,16 @@
     // 완료하기
     function todoComplete(target) {
         if(target) {
-            if(target.querySelector('input[type="checkbox"]').checked) {
+            const checkItem = target.querySelector('input[type="checkbox"]');
+            const key = checkItem.id.split('todo')[1];
+            const txt = target.querySelector('.txt').innerText;
+            if(checkItem.checked) {
                 target.classList.add('complete');
             } else {
                 target.classList.remove('complete');
             }
+            // 변경값 업데이트
+            setStorage(key, txt, checkItem.checked);
         }
     }
     
